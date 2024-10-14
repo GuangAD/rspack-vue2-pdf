cc00c832202af70f69a39f898adfbeb6be24f8dd
gaoguangce <1536576466@qq.com>
2024-10-14 13:58:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rspack-vue2-pdf
+Rspack-vue2-pdf is adapted to the rspack bundler by changing the [vue-pdf](https://www.npmjs.com/package/vue-pdf) source code
---
+++ src/vuePdfNoSss.vue
@@ -0,0 +1,40 @@
+<style src="./annotationLayer.css"></style>
+<script>
+// 	import componentFactory from './componentFactory.js'
+
+// 	if ( process.env.VUE_ENV !== 'server' ) {
+
+// 		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
+// 		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
+
+// 		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
+
+// 			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+// 			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+// 		}
+
+// 		var component = componentFactory(pdfjsWrapper(PDFJS));
+// 	} else {
+
+// 		var component = componentFactory({});
+// 	}
+
+// export default component;
+
+import componentFactory from "./componentFactory.js";
+import pdfjsWrapper from "./pdfjsWrapper.js";
+import PdfjsWorker from "worker-rspack-loader!pdfjs-dist/es5/build/pdf.worker.js";
+const PDFJS = require("pdfjs-dist/es5/build/pdf.js");
+
+if (
+  typeof window !== "undefined" &&
+  "Worker" in window &&
+  navigator.appVersion.indexOf("MSIE 10") === -1
+) {
+  PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+}
+
+var component = componentFactory(pdfjsWrapper(PDFJS));
+
+export default component;
+</script>
